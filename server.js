a3ceba45933f217c2c725876a7436de9596ff57d
hackagon <phonghiavan@gmail.com>
2020-04-17 10:55:28 +0700

    change port

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 // app.use("/api", require("./routes/user"));
 app.use("/api", require("./routes/api"))
 
-const port = 5000; ``
+const port = process.env.PORT || 5000; ``
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 })
