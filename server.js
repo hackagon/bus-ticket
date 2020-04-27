36e389b4d4fc455011f55041bc88e22dd58cd07e
hackagon <phonghiavan@gmail.com>
2020-04-27 15:38:37 +0700

    prevent cors

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ mongoose.connect("mongodb+srv://admin:admin@cluster0-h96u9.mongodb.net/bus-ticke
 // Tao instance
 // Luu instance ==> DB (document)
 
+
 const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  next();
+});
 
 app.use(express.json());
 
